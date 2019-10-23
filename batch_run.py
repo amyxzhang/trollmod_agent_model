b2666da56b7a202179b5b6f970dd64fb568f5de1
Amy Zhang <axz@mit.edu>
2019-10-23 16:43:59 -0700

    histogram batch

--- batch_run.py
+++ batch_run.py
@@ -3,19 +3,20 @@ from agentmodel.model import *
 from mesa.datacollection import DataCollector
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 fixed_params = {'num_agents': 50}
 
-variable_params = {'percent_trolls': [.1,.2,.3],
-                   'percent_mods': [.1,.2,.3],
+variable_params = {'percent_trolls': np.arange(0.0, 0.5, 0.05),
+                   'percent_mods': np.arange(0.0, 0.5, 0.05),
                    }
 
 batch_run = BatchRunner(TrollModNetwork,
                         variable_params,
                         fixed_params,
-                        iterations=5,
-                        max_steps=100,
+                        iterations=3,
+                        max_steps=40,
                         model_reporters={"average_trolling": compute_troll_avg})
 
 
@@ -24,7 +25,11 @@ if __name__ == '__main__':
     batch_run.run_all()
     run_data = batch_run.get_model_vars_dataframe()
     run_data.head()
-    plt.scatter(run_data.percent_trolls, run_data.average_trolling)
+    plt.scatter(run_data.percent_trolls, run_data.percent_mods, c=run_data.average_trolling)
+    cbar = plt.colorbar()
+    cbar.set_label("Average trolled")
+    plt.xlabel('Percent trolls')
+    plt.ylabel('Percent mods')
     plt.show()
     
     
